bb7be66ac6456c6657ded9919a18af9e6d41ea99
cristian <cristianpulga13@gmail.com>
2022-10-03 11:40:49 -0500

    js arreglos

---
+++ main.js
@@ -0,0 +1,23 @@
+let campo=document.getElementById('campo')
+let boton=document.getElementById('boton')
+
+const colores=[];
+boton.addEventListener("click", ()=>{
+  
+    colores.push(campo.value);
+   
+for (const listas of colores){
+    ul.innerHTML+=`<li>${listas}</li>`
+   
+}
+
+    
+
+});
+
+
+
+
+
+
+
---
+++ visual.js
@@ -0,0 +1,13 @@
+let figuras=["cuadrado"];
+
+figuras.push("circulo")
+
+
+figuras.push("triangulo")
+
+figuras.unshift("rombo")
+
+alert (figuras);
+
+
+alert(figuras.toString())
